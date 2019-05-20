7c537bdbaa7dad3b3b8b9c68e92ef43aa614560d
AleConcetti <Sandrino96>
2019-05-20 17:23:25 +0200

    start

--- scripts/firstScript.py
+++ scripts/firstScript.py
@@ -1,33 +1,24 @@
 import os
 
-#cmd = "gcc -I utilities -I linear-algebra/kernels/ utilities/polybench.c linear-algebra/kernels/atax/atax.c -o atax_base"
-
-PATH_POLYBENCH = "benchmark/polybench-c-3.2/"
+# gcc -O3 -I utilities -I linear-algebra/kernels/atax utilities/polybench.c linear-algebra/kernels/atax/atax.c -DPOLYBENCH_TIME -o atax_time
+PATH_POLYBENCH = "../benchmark/polybench-c-3.2/"
 #os.system("cd "+PATH_POLYBENCH+"&& ls")
+
 
 
 def funct(startpath,pippo):
     for root,dirs,files in os.walk(startpath):
         if (len(dirs)==0):
             for file in files:
-                pippo =
                 cmd = "gcc -I utilities -I linear-algebra/kernels/ utilities/polybench.c linear-algebra/kernels/atax/atax.c -o atax_base"
                 os.sys(cmd)
                 pass
         else:
             for dir in dirs:
                 funct(startpath+"/"+dir)
-
-
-
 def list_files(startpath):
     for root, dirs, files in os.walk(startpath):
         print(root,dirs, files)
-        # level = root.replace(startpath, '').count(os.sep)
-        # indent = ' ' * 4 * (level)
-        # print('{}{}/'.format(indent, os.path.basename(root)))
-        # subindent = ' ' * 4 * (level + 1)
-        # for f in files:
-        #     print('{}{}'.format(subindent, f))
 
-funct(PATH_POLYBENCH)
+
+list_files(PATH_POLYBENCH)
